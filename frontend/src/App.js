ff163223c42312af750a11f522829b4d5928d045
Jesper Lähdevirta <jesper.lahdevirta@fortnox.se>
2022-05-12 12:46:18 +0200

    removed spaces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
 import { Component } from 'react';
 
-
 class App extends Component {
 	state = {
 		message: "If spring backend is running and database is running, then this message should be replaced!"
 	};
-
 	async componentDidMount() {
 		await this.getHelloWorld();
 	}
-
 	async getHelloWorld() {
 		const response           = await fetch('/helloworld');
 		const helloWorldResponse = await response.json();
 		this.setState({ message: helloWorldResponse.message });
 	}
-
 	render() {
 		return (
 			<div className="App">
