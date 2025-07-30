2a232bfe0b835087bfe93f59705a10a27500d2bf
valle1336 <7alexxi@gmail.com>
2025-07-30 12:37:54 +0200

    Comments on RentalsRepository.java

--- src/main/java/com/example/rental/repositories/RentalsRepository.java
+++ src/main/java/com/example/rental/repositories/RentalsRepository.java
@@ -16,11 +16,13 @@ public class RentalsRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    //Välj alla kolumner från Rentals table
     public List<RentalsEntity> findAll() {
         String sql = "SELECT * FROM rentals";
         return jdbcTemplate.query(sql, new RentalsRowMapper());
     }
 
+    //Lägg till ny row i rentals med dynamiska värden från användaren (frontend)
     public void addRental(RentalsEntity rental) {
         String sql = "INSERT INTO rentals (car_id, driver_name, driver_age, start_date, end_date, revenue) VALUES (?, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(
@@ -33,8 +35,13 @@ public class RentalsRepository {
                 rental.getRevenue()
         );
     }
+
+    //Returnerar true om bilen är tillgänglig under det angivna datumintervallet
     public boolean isCarAvailable(int carId, LocalDate startDate, LocalDate endDate) {
+        //SELECT COUNT(*) ger oss numeriskt värde på hur många objekt rentals table innehåller
         String sql = "SELECT COUNT(*) FROM rentals " +
+                //Sedan letar vi efter alla bokningar numeriskt på ett specifikt car_id
+                //Sedan kollar vi datumen användaren försöker boka emot bilen som försöker bokas
                 "WHERE car_id = ? AND NOT (end_date < ? OR start_date > ?)";
         Integer count = jdbcTemplate.queryForObject(
                 sql,
@@ -43,15 +50,22 @@ public class RentalsRepository {
                 startDate,
                 endDate
         );
+        //Om inget resultat hittas (count = 0), är bilen ledig → return true
+        //Om count > 0, finns en överlappning → return false
         return count == null || count == 0;
     }
 
+
+    //Väldigt lik tidigare exempel
     public LocalDate nextAvailableDate(int carId, LocalDate startDate, LocalDate endDate) {
+        //Skillnaden här är SELECT MAX vi hämtar senaste end_date på ett objekt (Kan finnas flera bokningar vi vill ha senaste end_date på objektet)
         String sql = "SELECT MAX(end_date) FROM rentals WHERE car_id = ? AND NOT (end_date < ? OR start_date > ?)";
         LocalDate latestEnd = jdbcTemplate.queryForObject(
                 sql, LocalDate.class, carId, startDate, endDate
         );
 
+        //Om det finns en krockande bokning returnerar vi end_date + 1 för att visa tidigaste nästa boknings start
+        //Annars är bilen ledig direkt → returnera startDate
         return latestEnd != null ? latestEnd.plusDays(1) : startDate;
     }
 
